4a754761d8bf036bb942e46cfffc800bdbfa5def
Rahul Biswas <rahulbiswas1107@gmail.com>
2026-04-25 16:58:22 +0000

    Add multip.java to demonstrate multiplication

---
+++ Java/Basics/multip.java
@@ -0,0 +1,9 @@
+public class multip{
+	public static void main(String[] args){
+		int num = 100;
+		System.out.println("The number is :"+num);
+		num = num * 2;
+		System.out.print("The value of number * 2 is :");
+		System.out.println(num);
+	}
+}
